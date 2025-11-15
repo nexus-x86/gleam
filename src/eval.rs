4b8dfd3078937f8e5dfa00862981122e133b1abf
nexus-x86 <60083031+nexus-x86@users.noreply.github.com>
2025-11-15 18:29:51 -0500

    piece square table

--- src/eval.rs
+++ src/eval.rs
@@ -6,24 +6,116 @@ use cozy_chess::*;
 
 pub type EvalInt = i32;
 
-fn get_val(piece: Piece) -> EvalInt {
+type psqt = [EvalInt;64];
+
+// Piece Square Tables from the pov of White
+const pawn_pst : psqt = [
+    0,  0,  0,  0,  0,  0,  0,  0,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    5,  5, 10, 25, 25, 10,  5,  5,
+    0,  0,  0, 20, 20,  0,  0,  0,
+    5, -5,-10,  0,  0,-10, -5,  5,
+    5, 10, 10,-20,-20, 10, 10,  5,
+    0,  0,  0,  0,  0,  0,  0,  0
+];
+
+const knight_pst : psqt = [
+    -50,-40,-30,-30,-30,-30,-40,-50,
+    -40,-20,  0,  0,  0,  0,-20,-40,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -40,-20,  0,  5,  5,  0,-20,-40,
+    -50,-40,-30,-30,-30,-30,-40,-50,
+];
+
+const bishop_pst : psqt = [
+    -20,-10,-10,-10,-10,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5, 10, 10,  5,  0,-10,
+    -10,  5,  5, 10, 10,  5,  5,-10,
+    -10,  0, 10, 10, 10, 10,  0,-10,
+    -10, 10, 10, 10, 10, 10, 10,-10,
+    -10,  5,  0,  0,  0,  0,  5,-10,
+    -20,-10,-10,-10,-10,-10,-10,-20,
+];
+
+const rook_pst : psqt = [
+    0,  0,  0,  0,  0,  0,  0,  0,
+    5, 10, 10, 10, 10, 10, 10,  5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    0,  0,  0,  5,  5,  0,  0,  0
+];
+
+const queen_pst : psqt = [
+    -20,-10,-10, -5, -5,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5,  5,  5,  5,  0,-10,
+    -5,  0,  5,  5,  5,  5,  0, -5,
+    0,  0,  5,  5,  5,  5,  0, -5,
+    -10,  5,  5,  5,  5,  5,  0,-10,
+    -10,  0,  5,  0,  0,  0,  0,-10,
+    -20,-10,-10, -5, -5,-10,-10,-20
+];
+
+const king_pst : psqt = [
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -20,-30,-30,-40,-40,-30,-30,-20,
+    -10,-20,-20,-20,-20,-20,-20,-10,
+    20, 20,  0,  0,  0,  0, 20, 20,
+    20, 30, 10,  0,  0, 10, 30, 20
+];
+
+fn convertFile(file: File) -> usize {
+    if file == File::A {return 0;}
+    else if file == File::B {return 1;}
+    else if file == File::C {return 2;}
+    else if file == File::D {return 3;}
+    else if file == File::E {return 4;}
+    else if file == File::F {return 5;}
+    else if file == File::G {return 6;}
+    else {return 7;}
+}
+
+fn convertRank(rank: Rank) -> usize {
+    if rank == Rank::First {return 0;}
+    else if rank == Rank::Second {return 1;}
+    else if rank == Rank::Third {return 2;}
+    else if rank == Rank::Fourth {return 3;}
+    else if rank == Rank::Fifth {return 4;}
+    else if rank == Rank::Sixth {return 5;}
+    else if rank == Rank::Seventh {return 6;}
+    else {return 7;}
+}
+
+fn get_val(piece: Piece, index: usize) -> EvalInt {
+    
     if piece == Piece::Pawn {
-        return 100;
+        return 100 + pawn_pst[index];
     } 
     if piece == Piece::Knight {
-        return 250;
+        return 250 + knight_pst[index];
     }
     if piece == Piece::Bishop {
-        return 300;
+        return 300 + bishop_pst[index];
     }
     if piece == Piece::Rook {
-        return 500;
+        return 500 + rook_pst[index];
     }
     if piece == Piece::Queen {
-        return 900;
+        return 900 + queen_pst[index];
     }
     if piece == Piece::King {
-        return 20000;
+        return 20000 + king_pst[index];
     }
     return 0;
 }
@@ -43,9 +135,12 @@ impl Eval for Board {
         let squares = &Square::ALL;
         for (s1, &_s2) in bb.iter().zip(squares) {
             if self.color_on(s1) == Some(Color::White) {
-                white_score += get_val(self.piece_on(s1).unwrap());
+                let index = convertFile(s1.file()) * 8 + convertRank(s1.rank());
+    
+                white_score += get_val(self.piece_on(s1).unwrap(),index);
             } else if self.color_on(s1) == Some(Color::Black) {
-                black_score += get_val(self.piece_on(s1).unwrap());
+                let blackIndex = (7 - convertFile(s1.file())) * 8 + convertRank(s1.rank());
+                black_score += get_val(self.piece_on(s1).unwrap(), blackIndex);
             }
         }
 
